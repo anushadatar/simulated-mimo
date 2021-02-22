7ea9968f1076fa7b5d3bbaca71eb5739ac671bb3
Mark Goldwater <mgoldwater@olin.edu>
2021-02-22 09:29:15 -0500

    Calculating both weight vectors at once and splicing by row to return each

--- generate_weight_vector_mmse.m
+++ generate_weight_vector_mmse.m
@@ -1,16 +1,15 @@
-function w = generate_weight_vector_mmse(H, one_hot)
+function [w1, w2] = generate_weight_vector_mmse(H)
     % Generate the weight vector based on the channel (using the mmse
     % receiver).
     % Input Parameters:
     % H       : The aggregated channel parameter matrix.
-    % one_hot : The encoding for the specific receiver, with 1; 0
-    %           corresponding to receiver one and 0; 1 corresponding to 
-    %           receiver two.
     % Returns:
-    % w       : Weight vector associated with overall channel
-    %           characterization.
+    % w1       : Weight vector used to retrieve x1
+    % w2       : Weight vector used to retrieve x2
     
     % Estimated using var(real(y1(1:5000))).
     var = 0.0255;
-    w = inv(H'*H + var*eye(2, 2))*H'*one_hot;
+    w = inv(H'*H + var.*eye(2, 2))*H';
+    w1 = w(1, :);
+    w2 = w(2, :);
 end
